a698cb724188641bf553945e5a4f348d243a0ab7
gaveshvimanshana126-cyber <gaveshvimanshana126@gmail.com>
2026-02-19 19:48:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "b9I0mA4Y#l4D7s4LrzZDpqpE-Ot-HKYq-GaKzHgfET4BX902CJbA",
+SESSION_ID: process.env.SESSION_ID || "29B3TTbT#lkuTgYxb13y-3SayEq2CK18MW2gfKCh2JHT0eNdek1k",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/gaveshvimanshana126-cyber/VIMA-MD_V1/refs/heads/main/Image/20260218_134730.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 VIMA-MD Is Alive Now😍*",
 BOT_OWNER: '94742549935',  // Replace with the owner's phone number
